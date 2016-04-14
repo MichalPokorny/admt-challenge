b6e1e77ead650a402bd98e6f969107d409815d5c
Petr Hudeček <petrhudecek2010@gmail.com>
2016-04-14 18:46:31 +0200

    final version

--- main-project/src/Main.java
+++ main-project/src/Main.java
@@ -10,7 +10,15 @@ import java.util.stream.Collectors;
 import org.json.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private static boolean testPython() throws InterruptedException, IOException {
+
+        Process p = Runtime.getRuntime().exec("python", new String[] { "pythonfile.py" });
+        p.waitFor();
+
+        return true;
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+
 
         String mypyIssuesUrl = "https://api.github.com/repos/python/mypy/issues?per_page=100";
         URL mypyUrl = new URL(mypyIssuesUrl);
@@ -29,12 +37,27 @@ public class Main {
                     body.contains("fix");
 
             boolean containsCodeBlock =
-                    body.matches("(?s).*```.{10,}```.*");
+                    body.matches("(?s).*```(.{10,})```.*");
 
 
 
-            if (containsCodeBlock && talksAboutErrors)
-                 System.out.println(object.getString("title"));
+
+            if (containsCodeBlock && talksAboutErrors) {
+                System.out.println("[ISSUE] " + object.getString("title"));
+                Pattern pattern = Pattern.compile("(?s)```([^`]{10,})```");
+                Matcher matcher = pattern.matcher(body);
+                int mi = 0;
+                while (matcher.find()) {
+                    mi++;
+                    String thaCode = matcher.group(1);
+                    if (thaCode.startsWith("python")) {
+                        thaCode = thaCode.substring(6);
+                        System.out.println(">> CODE >> " + thaCode.replace('\n', ' ').replace('\r', ' ') );
+                    }
+
+
+                }
+            }
         }
 
 
